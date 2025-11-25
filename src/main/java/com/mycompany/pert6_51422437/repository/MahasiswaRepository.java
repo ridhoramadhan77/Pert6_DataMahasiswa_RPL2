fe9136bf9c40276f818a98f803ff718b379a2477
ridhoramadhan77 <ridhoramadhan987@gmail.com>
2025-11-25 16:23:31 +0700

    First Commit

---
+++ src/main/java/com/mycompany/pert6_51422437/repository/MahasiswaRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pert6_51422437.repository;
+
+import com.mycompany.pert6_51422437.model.ModelMahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Lenovo
+ */
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Integer> {
+
+}
